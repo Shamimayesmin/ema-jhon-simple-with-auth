6b0e00744a1806506e429899bd24c8a7b5020437
shamima <shamimayesmin144@gamil.com>
2022-10-17 17:29:39 +0600

    first commit

---
+++ src/__steps.js
@@ -0,0 +1,24 @@
+/**
+ 1. Auth setup:
+ 1. create firebase prokect
+ 2. enable web
+ 3. enable sign in method
+ 4. install firebase
+ 5. get firebase config in your firebase.js file
+ 6. export app from firebase.config.js 
+ 7.
+ * 
+ * 
+ */
+
+ /**
+  * 
+  1.create userContext (Auth context): userContext ---> component name, userContext provides AuthContext
+  2. create AuthContext
+  3. set AuthContext.provider
+  4. make sure you set the children
+  5. export AuthContext to be used inside useContext hook
+  6.get form data
+  7. getAuth in the UserContext
+  * 
+  */
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNq4q8E5OuAoCGAq0ioxTbiwoB3cvV2pY",
+  authDomain: "ema-jhon-simple-70dd7.firebaseapp.com",
+  projectId: "ema-jhon-simple-70dd7",
+  storageBucket: "ema-jhon-simple-70dd7.appspot.com",
+  messagingSenderId: "226001998599",
+  appId: "1:226001998599:web:05032f01271d1cd7132ba2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
